95ca5f2a20498b85fcc86c71c43c1c334b659eff
Precious <preciousmaregwa1997@gmail.com>
2022-06-12 13:26:51 +0300

    commit tree 9b739a65582d8e86846fd914292fbf5219b8e778

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_reader :word
+    def initialize(word)
+        @word = word
+    end
+
+    def match(arr)
+        arr.filter{|item| item.chars.sort == @word.chars.sort}
+    end
+end
